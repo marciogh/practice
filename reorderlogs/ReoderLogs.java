b5a26f00459b69d98d91d77afea109f76dce6ce2
Marcio Ghiraldelli <marcio.gh@gmail.com>
2025-05-27 10:34:04 +1000

    reorder logs

--- reorderlogs/ReoderLogs.java
+++ reorderlogs/ReoderLogs.java
@@ -3,14 +3,8 @@ package reorderlogs;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.TreeMap;
 
 import org.junit.Test;
 
@@ -30,7 +24,7 @@ public class ReoderLogs {
 
     private boolean isContentNumeric(String content) {
         for (String s : content.split(" ")) {
-            if (! isNumeric(s)) {
+            if (!isNumeric(s)) {
                 return false;
             }
         }
@@ -46,68 +40,69 @@ public class ReoderLogs {
             public int compare(String s1, String s2) {
                 String c1 = s1.substring(s1.indexOf(' ') + 1);
                 String c2 = s2.substring(s2.indexOf(' ') + 1);
-                if (! isContentNumeric(c1) && isContentNumeric(c2)) {
+                if (!isContentNumeric(c1) && isContentNumeric(c2)) {
                     return -1;
                 }
-                if (isContentNumeric(c1) && ! isContentNumeric(c2)) {
+                if (isContentNumeric(c1) && !isContentNumeric(c2)) {
                     return +1;
                 }
-                if (! isContentNumeric(c1) && ! isContentNumeric(c2)) {
-                    return s1.compareTo(s2);
+                if (!isContentNumeric(c1) && !isContentNumeric(c2)) {
+                    if (c1.equals(c2)) {
+                        return s1.compareTo(s2);
+                    }
+                    return c1.compareTo(c2);
                 }
                 return 0;
             }
         };
         Arrays.sort(logs, c);
         return logs;
-
-        /*
-        TreeMap<String, List<String>> letterLogs = new TreeMap<>();
-        HashMap<String, List<String>> digiLogs = new LinkedHashMap<>();
-        for (int i = 0; i < logs.length; i ++) {
-            String identifier = logs[i].substring(0, logs[i].indexOf(' '));
-            String content = logs[i].substring(logs[i].indexOf(' ') + 1);
-            if (isContentNumeric(content)) {
-                List<String> contents = digiLogs.getOrDefault(identifier, new ArrayList<String>());
-                contents.add(content);
-                digiLogs.put(identifier, contents);
-            } else {
-                List<String> identifiers = letterLogs.getOrDefault(content, new ArrayList<String>());
-                identifiers.add(identifier);
-                letterLogs.put(content, identifiers);
-            }
-        }
-        List<String> results = new ArrayList<>();
-        for (String key : letterLogs.keySet()) {
-            Collections.sort(letterLogs.get(key));
-            for (String identifier: letterLogs.get(key)) {
-                results.add(identifier + " " + key);
-            }
-        }
-        for (String key : digiLogs.keySet()) {
-            for (String content: digiLogs.get(key)) {
-                results.add(key + " " + content);
-            }
-
-        }        
-        return results.toArray(new String[results.size()]);
-        */
     }
+
+    /*
+     * TreeMap<String, List<String>> letterLogs = new TreeMap<>();
+     * HashMap<String, List<String>> digiLogs = new LinkedHashMap<>();
+     * for (int i = 0; i < logs.length; i ++) {
+     * String identifier = logs[i].substring(0, logs[i].indexOf(' '));
+     * String content = logs[i].substring(logs[i].indexOf(' ') + 1);
+     * if (isContentNumeric(content)) {
+     * List<String> contents = digiLogs.getOrDefault(identifier, new
+     * ArrayList<String>());
+     * contents.add(content);
+     * digiLogs.put(identifier, contents);
+     * } else {
+     * List<String> identifiers = letterLogs.getOrDefault(content, new
+     * ArrayList<String>());
+     * identifiers.add(identifier);
+     * letterLogs.put(content, identifiers);
+     * }
+     * }
+     * List<String> results = new ArrayList<>();
+     * for (String key : letterLogs.keySet()) {
+     * Collections.sort(letterLogs.get(key));
+     * for (String identifier: letterLogs.get(key)) {
+     * results.add(identifier + " " + key);
+     * }
+     * }
+     * for (String key : digiLogs.keySet()) {
+     * for (String content: digiLogs.get(key)) {
+     * results.add(key + " " + content);
+     * }
+     * 
+     * }
+     * return results.toArray(new String[results.size()]);
+     */
 
     public static void main(String[] args) {
         ReoderLogs r = new ReoderLogs();
-        String[] result = (
-            r.reorderLogFiles(new String[]
-                {
-                    "a1 9 2 3 1",
-                    "g1 act car",
-                    "zo4 4 7",
-                    "ab1 off key dog",
-                    "a8 act zoo",
-                    "a2 act car",
-                }
-            )
-        );
+        String[] result = (r.reorderLogFiles(new String[] {
+                "a1 9 2 3 1",
+                "g1 act car",
+                "zo4 4 7",
+                "ab1 off key dog",
+                "a8 act zoo",
+                "a2 act car",
+        }));
         System.out.println(result);
     }
 
@@ -115,68 +110,62 @@ public class ReoderLogs {
     public void test() {
         ReoderLogs r = new ReoderLogs();
 
-        assertEquals(0, r.reorderLogFiles(new String[]{}).length);
+        assertEquals(0, r.reorderLogFiles(new String[] {}).length);
 
-        String[] l1 = new String[]{
+        String[] l1 = new String[] {
                 "let1 art can",
+                "let3 art zero",
                 "let2 own kit dig",
-                "let3 art zero",                
                 "dig1 8 1 5 1",
                 "dig2 3 6",
-            };
+        };
         String[] l2 = r.reorderLogFiles(
-                new String[]{
-                    "dig1 8 1 5 1",
-                    "let1 art can",
-                    "dig2 3 6",
-                    "let2 own kit dig",
-                    "let3 art zero",
-                }                
-            );
+                new String[] {
+                        "dig1 8 1 5 1",
+                        "let1 art can",
+                        "dig2 3 6",
+                        "let2 own kit dig",
+                        "let3 art zero",
+                });
         assertArrayEquals(
-            l1,
-            l2
-        );
+                l1,
+                l2);
 
         assertArrayEquals(
-            new String[]{
-                "1 n u",
-                "r 527",
-                "j 893",
-                "6 14",
-                "6 82",
-            },
-            r.reorderLogFiles(
-                new String[]{
-                    "1 n u",
-                    "r 527",
-                    "j 893",
-                    "6 14",
-                    "6 82",
-                }                
-            )
-        );
+                new String[] {
+                        "1 n u",
+                        "r 527",
+                        "j 893",
+                        "6 14",
+                        "6 82",
+                },
+                r.reorderLogFiles(
+                        new String[] {
+                                "1 n u",
+                                "r 527",
+                                "j 893",
+                                "6 14",
+                                "6 82",
+                        }));
 
         assertArrayEquals(
-            new String[]{
-                "a2 act car",
-                "g1 act car",
-                "a8 act zoo",
-                "ab1 off key dog",
-                "a1 9 2 3 1",
-                "zo4 4 7",
-            },
-            r.reorderLogFiles(
-                new String[]{
-                    "a1 9 2 3 1",
-                    "g1 act car",
-                    "zo4 4 7",
-                    "ab1 off key dog",
-                    "a8 act zoo",
-                    "a2 act car",
-                }                
-            )
-        );        
+                new String[] {
+                        "a2 act car",
+                        "g1 act car",
+                        "a8 act zoo",
+                        "ab1 off key dog",
+                        "a1 9 2 3 1",
+                        "zo4 4 7",
+                },
+                r.reorderLogFiles(
+                        new String[] {
+                                "a1 9 2 3 1",
+                                "g1 act car",
+                                "zo4 4 7",
+                                "ab1 off key dog",
+                                "a8 act zoo",
+                                "a2 act car",
+                        }));
     }
 
     @Test
@@ -186,24 +175,33 @@ public class ReoderLogs {
             public int compare(String s1, String s2) {
                 String c1 = s1.substring(s1.indexOf(' ') + 1);
                 String c2 = s2.substring(s2.indexOf(' ') + 1);
-                if (! isContentNumeric(c1) && isContentNumeric(c2)) {
+                if (!isContentNumeric(c1) && isContentNumeric(c2)) {
                     return -1;
                 }
-                if (isContentNumeric(c1) && ! isContentNumeric(c2)) {
+                if (isContentNumeric(c1) && !isContentNumeric(c2)) {
                     return +1;
                 }
-                if (! isContentNumeric(c1) && ! isContentNumeric(c2)) {
+                if (!isContentNumeric(c1) && !isContentNumeric(c2)) {
                     return s1.compareTo(s2);
                 }
                 return 0;
             }
         };
-        String[] logs = new String[]{"abc dad", "cde cda", "kkk 123", "abc asd"};
+        String[] logs = new String[] {
+                "abc dad",
+                "cde cda",
+                "kkk 123",
+                "abc asd"
+        };
         Arrays.sort(logs, c);
         assertArrayEquals(
-            new String[]{"abc asd", "abc dad", "cde cda", "kkk 123"},
-            logs
-        );
+                new String[] {
+                        "abc asd",
+                        "abc dad",
+                        "cde cda",
+                        "kkk 123"
+                },
+                logs);
     }
 
 }
